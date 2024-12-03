26c58bc0edf9c06382955c712aabd06ee56f2299
0007Q <dominic.colson@hotmail.co.uk>
2024-12-03 16:02:24 +0000

    added face

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -96,7 +96,7 @@ const Sidebar = () => {
                   alt="profile-user"
                   width="100px"
                   height="100px"
-                  src={`../../assets/pandaface.jpg`}
+                  src={`../../assets/panda-bear.jpg`}
                   style={{ cursor: "pointer", borderRadius: "50%" }}
                 />
               </Box>
